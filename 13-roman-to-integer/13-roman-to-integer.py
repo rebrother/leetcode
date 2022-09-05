b8e82179de4346055257048b7fc8a93cd88e7220
rebrother <101189338+rebrother@users.noreply.github.com>
2022-09-05 14:15:27 +0900

    Time: 82 ms (40.44%), Space: 14 MB (30.11%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.py
+++ 13-roman-to-integer/13-roman-to-integer.py
@@ -1,15 +1,23 @@
 class Solution:
+    Sym = {
+    'I' : 1,
+    'V' : 5,
+    'X' : 10,
+    'L' : 50,
+    'C' : 100,
+    'D' : 500,
+    'M' : 1000
+    }
     def romanToInt(self, s:str):
-        sum = 0
-        Sym = {'I' : 1, 'V' : 5, 'X' : 10, 'L' : 50, 'C' : 100, 'D' : 500, 'M' : 1000}
+        result = 0
         s = list(s)
         for i in s:
-            sum += Sym[i]
+            result += self.Sym[i]
         for i in range(len(s)-1):
             if s[i] == 'I' and (s[i+1] == "V" or s[i+1] == "X"):
-                sum-=2
+                result-=2
             elif s[i] == 'X' and (s[i+1] == "L" or s[i+1] == "C"):
-                sum-=20
+                result-=20
             elif s[i] == 'C' and (s[i+1] == "D" or s[i+1] == "M"):
-                sum-=200
-        return sum
+                result-=200
+        return result
